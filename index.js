9fdf618c7b57e405fb8dee2036ac4d86c26dbd21
Moses Mbuki Mutitu <moses.mutitu@student.moringaschool.com>
2025-03-08 22:50:03 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// random
+const random = Math.floor(Math.random() * 100) + 1;
+
+// mod
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// max
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers);
